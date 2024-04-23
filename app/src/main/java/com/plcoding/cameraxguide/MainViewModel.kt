ca67f43d4d83d9c51849c5c969199234b3a9af05
Matheus Biscalchim <matheusbiscalchimmm@gmail.com>
2024-04-23 19:53:19 -0300

    sepya,negativo

---
+++ app/src/main/java/com/plcoding/cameraxguide/MainViewModel.kt
@@ -0,0 +1,28 @@
+package com.plcoding.cameraxguide
+
+import android.graphics.Bitmap
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class MainViewModel: ViewModel() {
+
+    private val _bitmaps = MutableStateFlow<List<Bitmap>>(emptyList())
+    val bitmaps = _bitmaps.asStateFlow()
+
+    fun onTakePhoto(bitmap: Bitmap) {
+        _bitmaps.value += bitmap
+    }
+
+    fun onTakeGreyScalePhoto(bitmap: Bitmap) {
+        _bitmaps.value += bitmap
+    }
+
+    fun onTakeSepyaPhoto(bitmap: Bitmap) {
+        _bitmaps.value += bitmap
+    }
+
+    fun onTakeNegativePhoto(bitmap: Bitmap) {
+        _bitmaps.value += bitmap
+    }
+}
